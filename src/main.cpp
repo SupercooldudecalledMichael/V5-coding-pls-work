656913ef6879f6cc1b4e1729a73cfaaa7c4309d4
SupercooldudenamedMichael <mikeneward@gmailcom>
2026-01-09 14:26:22 -0800

    hi?

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "helpers.hpp"
 #include "globals.hpp"
+#include "pros/motors.h"
 
 /*
 git add .
@@ -33,17 +34,31 @@ void initialize() {
 void disabled() {}
 void competition_initialize() {}
 void autonomous() {
- // set position to x:0, y:0, heading:0
     chassis.setPose(0, 0, 0);
-    chassis.moveToPose(-2, 13, -47, 3000,{}, false);
+    //auto win point
+    //chassis.moveToPose(-2, 13, -47, 3000,{}, false);
+    //intake();
+    //chassis.moveToPose(-16, 21.75, -57, 2000);
+    //chassis.moveToPose(-43.5, 1, 1, 3000);
+    //top_piston.set_value(true);
+    //chassis.moveToPose(-40.75,15,1,2000, {}, false);
+    //highScore();
+    
+    //auto skills
+    chassis.moveToPose(2,32.25,-90,4000);
+    bttmDown();
+    chassis.moveToPose(-15,30,-90,4000, {.minSpeed=50});
     intake();
-    chassis.moveToPose(-15, 20.75, -57, 2000);
-    chassis.moveToPose(-47, 1, 0, 3000);
-    top_piston.set_value(true);
-    chassis.moveToPose(-43.25,14,0,2000, {}, false);
-    highScore();
-    // turn to face heading 90 with a very long timeout
-
+    chassis.moveToPose(-14,30,-90,500, {.minSpeed=50});
+    chassis.moveToPose(-15,30,-90,500, {.minSpeed=50});
+    chassis.moveToPose(-14,30,-90,500, {.minSpeed=50});
+    chassis.moveToPose(-15,30,-90,500, {.minSpeed=50});
+    chassis.moveToPose(13, 30, 240, 4000, {.forwards=false, .minSpeed=40});
+    chassis.moveToPose(-3, -62, 270.5, 4000, {.minSpeed=40});
+    chassis.moveToPose(-14.75, -62, 270.5, 4000, {.minSpeed=40});
+    intake();
+    //auoto skills
+    //chassis.moveToPoint(0, 40, 9000,  {.forwards = true, .minSpeed = 127});
 }
 
 
